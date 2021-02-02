65abfd0c5debd43d73fa7c8a1713816e7f8371b7
Austin Schrader <austin.jschrader@gmail.com>
2021-02-02 10:38:10 -0800

    Add npm install

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     new UglifyJsPlugin({ sourceMap: true }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Plant Project',
+      title: 'Water Plant Project',
       template: './src/index.html',
       inject: 'body'
     })
